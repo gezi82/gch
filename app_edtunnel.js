b0003a96fb5b870a1ba0e28a3a0b54ddb37d96a3
gezi82 <gezi82@outlook.com>
2024-12-19 10:24:23 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bf8ee11b-1208-404b-8a79-be17b970523f';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
